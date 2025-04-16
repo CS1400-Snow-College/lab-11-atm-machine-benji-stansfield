b7c7ea85978aa209cda7b370dcff55fafc6f4348
Benji Stansfield <benji.stansfield@students.snow.edu>
2025-04-15 18:43:20 -0600

    Login created, kicks out if too many attempts

--- Program.cs
+++ Program.cs
@@ -2,11 +2,36 @@
 Console.Clear();
 
 string[] lines = File.ReadAllLines("bank.txt");
-foreach(string line in lines)
-{
-    string[] parts = line.Split(',');
+int attempts = 3;
+bool loggedIn = false;
 
-    string username = parts[0];
-    string pin = parts[1];
-    int balance = Convert.ToInt32(parts[2]);
+while (!loggedIn && attempts > 0)
+{
+    Console.Write("Username: ");
+    string usernameInput = Console.ReadLine();
+    Console.Write("Pin #: ");
+    string pinInput = Console.ReadLine();
+
+    foreach(string line in lines)
+    {
+        string[] parts = line.Split(',');
+
+        string username = parts[0];
+        string pin = parts[1];
+        string balance = parts[2];
+
+        if (usernameInput == username && pinInput == pin)
+        {
+            Console.Write("Sign in successful");
+            loggedIn = true;
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Username or pin not recognized.");
+            attempts--;
+            Console.WriteLine($"{attempts} attempts remaining");
+            break;
+        }
+    }
 }
--- obj/Debug/net8.0/lab-11-atm-machine-benji-stansfield.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab-11-atm-machine-benji-stansfield.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-11-atm-machine-benji-stansfield")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dddc3070dc2cc5fd00b118ba0760a451af1fc6e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c095d8b2a34481ed4647ce647d73bedf09755593")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-11-atm-machine-benji-stansfield")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-11-atm-machine-benji-stansfield")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

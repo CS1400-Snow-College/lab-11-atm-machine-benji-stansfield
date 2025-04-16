94afbffb34aa8bb4ba36d5e299dace3cc7695328
Benji Stansfield <benji.stansfield@students.snow.edu>
2025-04-15 23:25:26 -0600

    Rough draft of methods written (doesn't run)

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿/*Benji Stansfield, 04-09-25*/
+﻿/*Benji Stansfield, 04-09-25, Lab 11 "ATM Machine"*/
 Console.Clear();
 
 string[] lines = File.ReadAllLines("bank.txt");
@@ -36,7 +36,7 @@ while (!loggedIn && attempts > 0)
     }
 }
 
-if (loggedIn)
+while(loggedIn)
 {
     Console.WriteLine(@"
     1 - Check Balance
@@ -45,5 +45,77 @@ if (loggedIn)
     4 - Display last 5 transactions
     5 - Quick Withdraw $40
     6 - Quick Withdraw $100
-    7 - End current session");
+    7 - End current session
+    ");
+    Console.Write("What would you like to do? (type 1-7): ");
+    int menuSelection = Convert.ToInt32(Console.ReadLine());
+
+    switch (menuSelection)
+    {
+        case 1:
+            CheckBalance();
+            break;
+    }
+
+}
+
+static void CheckBalance()
+{
+    Console.WriteLine($"Your current balance is {parts[2]}.");
+}
+
+static void Withdraw()
+{
+    Console.Write("How much money would you like to withdraw?: ");
+    int withdrawalAmmount = Convert.ToInt32(Console.ReadLine());
+    if (withdrawalAmmount <= balance)
+    {
+        balance = balance - withdrawalAmmount;
+        Console.Write($"You have taken out ${withdrawalAmmount}. Your remaining balance is {balance}.");
+        return;
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient funds.");
+        return;
+    }
+}
+
+static void Deposit()
+{
+    Console.Write("How much money would you like to deposit?: ");
+    int depositAmmount = Convert.ToInt32(Console.ReadLine());
+    balance = balance + depositAmmount;
+    Console.Write($"You have deposited ${depositAmmount}. Your remaining balance is {balance}.");
+    return;
+}
+
+static void QuickWithdraw40()
+{
+    if (balance >= 40)
+    {
+        balance = balance - 40;
+        Console.WriteLine($"Transaction successful. Remaining balance is ${balance}.");
+        return;
+    }
+    else
+    {
+        Console.WriteLine("Insufficient funds.");
+        return;
+    }
+}
+
+static void QuickWithdraw100()
+{
+    if (balance >= 100)
+    {
+        balance = balance - 100;
+        Console.WriteLine($"Transaction successful. Remaining balance is ${balance}.");
+        return;
+    }
+    else
+    {
+        Console.WriteLine("Insufficient funds.");
+        return;
+    }
 }

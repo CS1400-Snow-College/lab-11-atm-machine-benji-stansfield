c095d8b2a34481ed4647ce647d73bedf09755593
Benji Stansfield <benji.stansfield@students.snow.edu>
2025-04-15 18:12:49 -0600

    Split the file into parts

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿/*Benji Stansfield, 04-09-25*/
 Console.Clear();
+
+string[] lines = File.ReadAllLines("bank.txt");
+foreach(string line in lines)
+{
+    string[] parts = line.Split(',');
+
+    string username = parts[0];
+    string pin = parts[1];
+    int balance = Convert.ToInt32(parts[2]);
+}
